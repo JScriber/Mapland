c7c9d836bf37008826109ff61936b1af79d38571
Jscriber <anthonygst26@gmail.com>
2018-02-14 13:41:17 +0100

    Favicons

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
 		<link rel="stylesheet" href="styles/main.css" type="text/css"/>
 		<link rel="stylesheet" href="styles/finder.css" type="text/css"/>
 		<link rel="stylesheet" href="styles/accueil.css" type="text/css"/>
-		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-		<link rel="shortcut icon" href="images/system/logos/favicon.png">
-		<meta charset="utf-8" />
+		
+		<!-- Favicon -->
+		<link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+		<link rel="manifest" href="images/favicons/site.webmanifest">
+		<link rel="mask-icon" href="images/favicons/safari-pinned-tab.svg" color="#6a1b9a">
+		<link rel="shortcut icon" href="images/favicons/favicon.ico">
+		<meta name="msapplication-TileColor" content="#6a1b9a">
+		<meta name="msapplication-config" content="images/favicons/browserconfig.xml">
+		<meta name="theme-color" content="#6a1b9a">
+
+		<meta charset="utf-8"/>
 		<title>MapLand</title>
 	</head>
 
